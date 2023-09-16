b5cf3177874ed030ad7a9240d29f5b5cf8c1a7b7
Alexander <a.ayugov@zebrains.team>
2023-09-16 15:41:36 +0400

    adding some changes

--- src/components/ChartDisplay/ChartDisplay.jsx
+++ src/components/ChartDisplay/ChartDisplay.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Bar } from 'react-chartjs-2';
+import { Bar, Line} from 'react-chartjs-2';
 import { Chart as ChartJS } from 'chart.js/auto';
 import styles from './ChartDisplay.module.scss'
 
 
 function ChartDisplay({ data }) {
   const chartData = {
+    title: 'fdfdf',
     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [
-      {
+      { 
         label: 'Данные',
         backgroundColor: 'rgba(75, 192, 192, 0.2)',
         borderColor: 'rgba(75, 192, 192, 1)',
@@ -18,12 +19,25 @@ function ChartDisplay({ data }) {
     ],
   };
 
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Название графики',
+      },
+    },
+  };
+
 
 
   return (
     <div className={styles.graphics}>
-      <h2 className={styles.title}>Отображение графика</h2>
-      <Bar className={styles.graph} data={chartData} />
+      <h2 className={styles.title}>Отображение графики</h2>
+      <Bar  className={styles.graph} options={options} data={chartData} />
     </div>
   );
 }
